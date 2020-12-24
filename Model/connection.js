a86c9b2d797a91f5a323f01f2965877a8b1aa60c
sumitpant <38173704+sumitpant@users.noreply.github.com>
2020-12-24 22:51:20 +0530

    crud api completed

--- Model/connection.js
+++ Model/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const modelSchema=require('./ModelLogin')
 const uri="mongodb+srv://Sumit:QCba1mVgf2kCNwxh@cluster0.cauj0.mongodb.net/loginuser?retryWrites=true&w=majority"
+// ye pura h na  haan yhee h
 mongoose.connect(process.env.MONGODB_URI||uri, {useNewUrlParser: true});
 
 const db = mongoose.connection;
@@ -11,7 +12,7 @@ const model2=mongoose.model('reserved',modelSchema.usersSchema)
 
 db.on('error',()=>{console.log("error")});
 db.once('open',()=>{
-    console.log("connected")
+    console.log("connected")//ye console me ajjata tha jb connect hota tha tb to local me dekhta hoga na terminal print hota hoaaa
 
 //  model.find({},(err,data)=>{
 //      console.log(data)
